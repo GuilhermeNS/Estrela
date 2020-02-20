8bbb3036bb60d08006b09fc618196e3f81254546
guilherme.nsantos7 <guilherme_nsantos2002@hotmail.com>
2020-02-20 12:22:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/estrela/Login_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.estrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/estrela/Splash_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.estrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
